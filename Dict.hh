430252321e68665319884ba35c1fc2697048b92a
wotan <wotan-notung@hotmail.fr>
2012-08-29 01:45:01 +0200

    Dict now throw exceptions

--- Dict.hh
+++ Dict.hh
@@ -6,6 +6,7 @@
 # include <boost/any.hpp>
 # include <boost/thread.hpp>
 
+# include "Exception.hh"
 # include "Thread.hh"
 
 namespace navi {
@@ -13,6 +14,7 @@ namespace navi {
 class Dict : public navi::ObjectLevelLockable<Dict>
 {
 public:
+  DEFINE_EXCEPTION(Error);
   typedef std::map<std::string const, boost::any> AnyMap;
   typedef AnyMap::iterator iterator;
   typedef AnyMap::const_iterator const_iterator;
@@ -38,8 +40,7 @@ public:
 
   template <typename T>
   T const get(std::string const& key) const {
-    Lock guard(this);
-    return boost::any_cast<T>(_map.at(key));
+    return boost::any_cast<T>((*this)[key]);
   }
 
   bool exist(std::string const& key) {
@@ -55,8 +56,12 @@ public:
 
   boost::any const& operator[](std::string const& str) const
   {
+    try {
     Lock guard(this);
     return _map.at(str);
+    } catch (std::exception&) {
+      throw Error("navi::Dict - key \'%s\' not found", str.c_str());
+    }
   }
 
   iterator begin()
