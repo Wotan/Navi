b11de96965aabfea93741dee0e4c54e2b3bef042
wotan <wotan-notung@hotmail.fr>
2012-09-10 16:24:16 +0200

    Fix Split

--- Split.hh
+++ Split.hh
@@ -10,11 +10,11 @@ namespace navi {
 template <typename T>
 void split(std::string const& str, std::vector<T>& vect, char sep = ' ')
 {
-  for (std::size_t i = 0; str[i]; ++i) {
-    for (;str[i] == sep; ++i);
+  for (std::size_t i = 0; i < str.size(); ++i) {
+    for (;str[i] == sep && i < str.size(); ++i);
     if (str[i]) {
       std::string pending;
-      for (;str[i] != sep && str[i]; ++i)
+      for (;str[i] != sep && i < str.size(); ++i)
 	pending.push_back(str[i]);
       vect.push_back(boost::lexical_cast<T, std::string>(pending));
     }
